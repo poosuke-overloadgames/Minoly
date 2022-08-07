8d9b3661cdf93a72e2646e46565905502264e5f3
poosuke <__poosuke@infoseek.jp>
2022-08-07 13:41:28 +0900

    追加：ObjectPostman異常系テスト

--- Assets/Scripts/Tests/WebClients/TestObjectPostman.cs
+++ Assets/Scripts/Tests/WebClients/TestObjectPostman.cs
@@ -15,8 +15,8 @@ namespace Tests
 		private const string UserName = "bbb";
 		private const int Score = 200;
 		private const string ContentInJson = "{\"userName\": \"bbb\", \"score\": \"200\"}";
-		
-		
+
+
 		[UnityTest]
 		public IEnumerator 正常系()
 		{
@@ -24,7 +24,7 @@ namespace Tests
 			var clientKey = EditorUserSettings.GetConfigValue("MinolyClientKey");
 			var objectGetter = new ObjectGetter(applicationKey, clientKey);
 			var objectPostman = new ObjectPostman(applicationKey, clientKey);
-			
+
 			yield return objectPostman.PostAsync(ClassName, ContentInJson);
 			var postResult = objectPostman.GetResult();
 			Assert.That(postResult.Type, Is.EqualTo(RequestResultType.Success));
@@ -42,6 +42,22 @@ namespace Tests
 			var testClass = JsonUtility.FromJson<TestClass>(getResult.Body);
 			Assert.That(testClass.userName, Is.EqualTo(UserName));
 			Assert.That(testClass.score, Is.EqualTo(Score));
+		}
+
+		[UnityTest]
+		public IEnumerator 異常系()
+		{
+			var applicationKey = "Detarame";
+			var clientKey = EditorUserSettings.GetConfigValue("MinolyClientKey");
+			var objectGetter = new ObjectGetter(applicationKey, clientKey);
+			var objectPostman = new ObjectPostman(applicationKey, clientKey);
+
+			yield return objectPostman.PostAsync(ClassName, ContentInJson);
+			var postResult = objectPostman.GetResult();
+			Assert.That(postResult.Type, Is.EqualTo(RequestResultType.ProtocolError));
+			Assert.That(postResult.HttpStatusCode, Is.EqualTo(404));
+			Assert.That(postResult.ErrorResponse.code, Is.EqualTo("E404005"));
+			Assert.That(postResult.ErrorResponse.error, Is.EqualTo("No such application."));
 		}
 	}
 }
