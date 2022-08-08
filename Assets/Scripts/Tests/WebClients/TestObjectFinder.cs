0479657a39a5407b09436f86520f8869449177ee
poosuke <__poosuke@infoseek.jp>
2022-08-09 08:41:49 +0900

    追加：limit句

--- Assets/Scripts/Tests/WebClients/TestObjectFinder.cs
+++ Assets/Scripts/Tests/WebClients/TestObjectFinder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Minoly;
 using NUnit.Framework;
@@ -13,17 +14,35 @@ namespace Tests
 	{
 		private const string ClassName = "TestClass";
 
+		private int CountOf(string src, string word)
+		{
+			var count = 0;
+			var index = 0;
+			while (true)
+			{
+				index = src.IndexOf(word, index + 1, StringComparison.Ordinal);
+				if (-1 == index) return count;
+				count++;
+			}
+		}
+
+
 		[UnityTest]
 		public IEnumerator 正常系()
 		{
 			var applicationKey = EditorUserSettings.GetConfigValue("MinolyApplicationKey");
 			var clientKey = EditorUserSettings.GetConfigValue("MinolyClientKey");
 			var objectFinder = new ObjectFinder(applicationKey, clientKey);
-			yield return objectFinder.FindAsync(ClassName, new[]{new QueryWhereEqualTo("userName", "aaa")});
+			yield return objectFinder.FindAsync(ClassName, new IQuery[]
+			{
+				new QueryWhereEqualTo("userName", "aaa"), 
+				new QueryLimit(2)
+			});
 			var result = objectFinder.GetResult();
 			Assert.That(result.Type, Is.EqualTo(RequestResultType.Success));
 			Assert.That(result.HttpStatusCode, Is.EqualTo(200));
 			Assert.That(result.ErrorResponse, Is.Null);
+			Assert.That(CountOf(result.Body, "objectId"), Is.EqualTo(2));
 		}
 
 	}
