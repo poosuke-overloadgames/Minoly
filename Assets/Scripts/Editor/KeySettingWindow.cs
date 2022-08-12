8d4427564bab1c9ce30837e0ea3844a2f66389d2
poosuke <__poosuke@infoseek.jp>
2022-08-12 16:04:56 +0900

    削除：KeySettingからMinolyObjectId

--- Assets/Scripts/Editor/KeySettingWindow.cs
+++ Assets/Scripts/Editor/KeySettingWindow.cs
@@ -7,7 +7,6 @@ namespace Editor
 	{
 		private string _applicationKey = "";
 		private string _clientKey = "";
-		private string _objectId = "";
 
 		[MenuItem("Tools/MinolyKeySetting")]
 		private static void ShowWindow()
@@ -19,7 +18,6 @@ namespace Editor
 		{
 			_applicationKey = EditorUserSettings.GetConfigValue("MinolyApplicationKey");
 			_clientKey = EditorUserSettings.GetConfigValue("MinolyClientKey");
-			_objectId = EditorUserSettings.GetConfigValue("MinolyObjectId");
 		}
 
 		private void OnGUI()
@@ -28,13 +26,10 @@ namespace Editor
 			_applicationKey = EditorGUILayout.TextField(_applicationKey);
 			GUILayout.Label("ClientKey");
 			_clientKey = EditorGUILayout.TextField(_clientKey);
-			GUILayout.Label("ObjectId (To Test ObjectGetter)");
-			_objectId = EditorGUILayout.TextField(_objectId);
 			if (GUILayout.Button("Apply"))
 			{
 				EditorUserSettings.SetConfigValue("MinolyApplicationKey", _applicationKey);
 				EditorUserSettings.SetConfigValue("MinolyClientKey", _clientKey);
-				EditorUserSettings.SetConfigValue("MinolyObjectId", _objectId);
 				AssetDatabase.SaveAssets(); 
 				Debug.Log("<color=#00ff00ff>Minoly KetSetting Applied!</color>");
 			}
