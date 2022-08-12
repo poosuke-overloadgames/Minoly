00c928c268b5997a42cd44316af8d47a44500e6d
poosuke <__poosuke@infoseek.jp>
2022-08-12 13:02:01 +0900

    追加：ObjectGetter.Dispose実装、テスト整理

--- Assets/Scripts/Tests/WebClients/TestObjectGetter.cs
+++ Assets/Scripts/Tests/WebClients/TestObjectGetter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using Minoly;
@@ -13,21 +14,43 @@ using UnityEngine.TestTools;
 namespace Tests
 {
 	[TestFixture]
-	public class TestGetObject
+	public class TestObjectGetter
 	{
 		private const string ClassName = "TestClass";
+		private const string ContentInJson = "{\"userName\": \"aaa\", \"score\": \"100\"}";
 		private const string UserName = "aaa";
 		private const int Score = 100;
+		private ObjectGetter _objectGetter;
+		private ObjectPostman _objectPostman;
+		private ObjectDeleter _objectDeleter;
+		private string _objectId;
+		
+		[UnitySetUp]
+		public IEnumerator SetUp()
+		{
+			var applicationKey = EditorUserSettings.GetConfigValue("MinolyApplicationKey");
+			var clientKey = EditorUserSettings.GetConfigValue("MinolyClientKey");
+			_objectGetter = new ObjectGetter(applicationKey, clientKey);
+			_objectPostman = new ObjectPostman(applicationKey, clientKey);
+			_objectDeleter = new ObjectDeleter(applicationKey, clientKey);
+			yield return _objectPostman.PostAsync(ClassName, ContentInJson);
+			_objectId = _objectPostman.GetResult().ObjectId;
+		}
+
+		[UnityTearDown]
+		public IEnumerator TearDown()
+		{
+			yield return _objectDeleter.DeleteAsync(ClassName, _objectId);
+			_objectDeleter.Dispose();
+			_objectGetter.Dispose();
+			_objectPostman.Dispose();
+		}
 
 		[UnityTest]
 		public IEnumerator 正常系()
 		{
-			var applicationKey = EditorUserSettings.GetConfigValue("MinolyApplicationKey");
-			var clientKey = EditorUserSettings.GetConfigValue("MinolyClientKey");
-			var objectId = EditorUserSettings.GetConfigValue("MinolyObjectId");
-			var objectGetter = new ObjectGetter(applicationKey, clientKey);
-			yield return objectGetter.FetchAsync(ClassName, objectId);
-			var result = objectGetter.GetResult();
+			yield return _objectGetter.FetchAsync(ClassName, _objectId);
+			var result = _objectGetter.GetResult();
 			Assert.That(result.Type, Is.EqualTo(RequestResultType.Success));
 			Assert.That(result.HttpStatusCode, Is.EqualTo(200));
 			Assert.That(result.ErrorResponse, Is.Null);
@@ -41,30 +64,26 @@ namespace Tests
 		{
 			var applicationKey = EditorUserSettings.GetConfigValue("MinolyApplicationKey");
 			var clientKey = "Detarame";
-			var objectId = EditorUserSettings.GetConfigValue("MinolyObjectId");
 			var objectGetter = new ObjectGetter(applicationKey, clientKey);
-			yield return objectGetter.FetchAsync(ClassName, objectId);
+			yield return objectGetter.FetchAsync(ClassName, _objectId);
 			var result = objectGetter.GetResult();
 			Assert.That(result.Type, Is.EqualTo(RequestResultType.ProtocolError));
 			Assert.That(result.HttpStatusCode, Is.EqualTo(403));
 			Assert.That(result.ErrorResponse.code, Is.EqualTo("E403002"));
 			Assert.That(result.ErrorResponse.error, Is.EqualTo("Unauthorized operations for signature."));
+			objectGetter.Dispose();
 		}
 
 		[UnityTest]
 		public IEnumerator 異常系_多重取得()
 		{
-			var applicationKey = EditorUserSettings.GetConfigValue("MinolyApplicationKey");
-			var clientKey = EditorUserSettings.GetConfigValue("MinolyClientKey");
-			var objectId = EditorUserSettings.GetConfigValue("MinolyObjectId");
-			var objectGetter = new ObjectGetter(applicationKey, clientKey);
-			var op1 = objectGetter.FetchAsync(ClassName, objectId);
+			var op1 = _objectGetter.FetchAsync(ClassName, _objectId);
 			UnityWebRequestAsyncOperation op2 = null;
 			var caught = false;
 			try
 			{
 				//objectGetterを新しく作る必要がある。
-				op2 = objectGetter.FetchAsync(ClassName, objectId);
+				op2 = _objectGetter.FetchAsync(ClassName, _objectId);
 			}
 			catch (MinolyInProgressException)
 			{
@@ -79,29 +98,20 @@ namespace Tests
 		[UnityTest]
 		public IEnumerator UniTaskによるFetch() => UniTask.ToCoroutine(async () =>
 		{
-			var applicationKey = EditorUserSettings.GetConfigValue("MinolyApplicationKey");
-			var clientKey = EditorUserSettings.GetConfigValue("MinolyClientKey");
-			var objectId = EditorUserSettings.GetConfigValue("MinolyObjectId");
-			var objectGetter = new ObjectGetter(applicationKey, clientKey);
-			var result = await objectGetter.FetchTask(ClassName, objectId);
+			var result = await _objectGetter.FetchTask(ClassName, _objectId);
 			Assert.That(result.Type, Is.EqualTo(RequestResultType.Success));
 			Assert.That(result.HttpStatusCode, Is.EqualTo(200));
 			Assert.That(result.ErrorResponse, Is.Null);
 			var testClass = JsonUtility.FromJson<TestClass>(result.Body);
 			Assert.That(testClass.userName, Is.EqualTo(UserName));
 			Assert.That(testClass.score, Is.EqualTo(Score));
-
 		});
 
 		[UnityTest]
 		public IEnumerator UniTaskによるFetchをキャンセル() => UniTask.ToCoroutine(async () =>
 		{
-			var applicationKey = EditorUserSettings.GetConfigValue("MinolyApplicationKey");
-			var clientKey = EditorUserSettings.GetConfigValue("MinolyClientKey");
-			var objectId = EditorUserSettings.GetConfigValue("MinolyObjectId");
 			var cts = new CancellationTokenSource();
-			var objectGetter = new ObjectGetter(applicationKey, clientKey);
-			var task = objectGetter.FetchTask(ClassName, objectId, null, PlayerLoopTiming.Update, cts.Token);
+			var task = _objectGetter.FetchTask(ClassName, _objectId, null, PlayerLoopTiming.Update, cts.Token);
 			cts.Cancel();
 			try
 			{
@@ -111,10 +121,12 @@ namespace Tests
 			{
 				//なぜかこの時点ではInProgressもある。
 				//OperationCanceledExceptionを投げるタイミングとキャンセル処理が終わるタイミングがリンクしてないっぽい
-				Assert.That(objectGetter.GetResult().Type, Is.EqualTo(RequestResultType.InProgress).Or.EqualTo(RequestResultType.Aborted));
-				await UniTask.Yield();
+				while (RequestResultType.InProgress == _objectGetter.GetResult().Type)
+				{
+					await UniTask.Yield();
+				}
 			}
-			var result = objectGetter.GetResult();
+			var result = _objectGetter.GetResult();
 			Assert.That(result.Type, Is.EqualTo(RequestResultType.Aborted));
 		});
 	}
